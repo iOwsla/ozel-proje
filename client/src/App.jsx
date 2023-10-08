bac6a1300b224e181153984e5b4278cbda4ffdc1
Owsla <81816429+iOwsla@users.noreply.github.com>
2023-10-08 03:46:20 +0300

    create header component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,9 +4,11 @@ import About from '~/pages/about';
 import Profile from '~/pages/Profile';
 import SignIn from './pages/SignIn';
 import SignOut from './pages/SignOut';
+import Header from './components/Header';
 function App() {
   return (
     <BrowserRouter>
+    <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sign-in" element={<SignIn />} />
